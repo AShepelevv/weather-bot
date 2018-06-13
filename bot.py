a7f459668deeda9544dcbd9943ba5b1b2eab12f4
AShepelevv <shepelev.as@phystech.edu>
2018-06-13 19:24:10 +0300

    refactored temp typing style

--- bot.py
+++ bot.py
@@ -28,13 +28,14 @@ def start(message):
         forecast = owm.three_hours_forecast(message.text).get_forecast()
 
         weather_text = 'It is {0}˚С in {1} now. The lowest temperature will be {2}˚С and the highest ' \
-            'will be {3}˚С.\n'.format(temp['temp'], message.text, temp['temp_min'], temp['temp_max']) + \
-            '\nThe atmospheric pressure is {0} mmHg.\n'.format('{:3.0f}'.format(pressure)) + \
-            '\nThe humidity is {0}%.\n'.format(humidity) + '\nWeather forecast:\n'
+                       'will be {3}˚С.\n'.format('{:+d}'.format(temp['temp']), message.text, temp['temp_min'],
+                                                 temp['temp_max']) + \
+                       '\nThe atmospheric pressure is {0} mmHg.\n'.format('{:3.0f}'.format(pressure)) + \
+                       '\nThe humidity is {0}%.\n'.format(humidity) + '\nWeather forecast:\n'
 
         i = 0
         for w in forecast:
-            if (i < 9 and  i > 0):
+            if (i < 9 and i > 0):
                 weather_text += '{:2d}:00'.format(w.get_reference_time(timeformat='date').hour) + \
                                 '{:5.0f}˚C\n'.format(w.get_temperature('celsius')['temp'])
             i += 1
